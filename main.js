ff691b0b8a3b81fc27ca70124e414cc571418453
whitehatjrafreennithin <78360402+whitehatjrafreennithin@users.noreply.github.com>
2021-04-08 01:07:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var inputs = [];
+
+function getParagraph1()
+{
+var inputs = [];
+for(var i = 1 ; i <=6 ; i++)
+{
+inputs.push(document.getElementById("div1_input_box_" + i).value);
+}
+document.getElementById("showParagraph1").innerHTML = inputs.join(". ");
+}
+
+function getParagraph2()
+{
+var inputs = [];
+for(var i = 1 ; i <=6 ; i++)
+{
+inputs.push(document.getElementById("div2_input_box_" + i).value);
+}
+document.getElementById("showParagraph2").innerHTML = inputs.join(". ");
+}
+
+
+    
+
+    console.log(inputs1);
+
+
+
+
+
+   var length_of_inputs1 = inputs1.length;
+    console.log(length_of_inputs1);
